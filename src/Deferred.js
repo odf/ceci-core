96e10638e5cf78189b182151c809a82315de6ec0
odf <olaf.delgado@gmail.com>
2014-01-30 10:48:14 +1100

    prepend underscores to 'private' Deferred methods and properties

--- src/Deferred.js
+++ src/Deferred.js
@@ -7,48 +7,49 @@ const REJECTED = 2;
 
 
 function Deferred() {
-  this.onResolve = null;
-  this.onReject  = null;
-  this.state     = PENDING;
-  this.value     = undefined;
+  this._onResolve = null;
+  this._onReject  = null;
+  this._state     = PENDING;
+  this._value     = undefined;
 };
 
+Deferred.prototype._update = function(state, val) {
+  if (this.isResolved())
+    throw new Error('deferred is already resolved');
+
+  this._state = state;
+  this._value = val;
+  this._publish();
+};
+
+Deferred.prototype._publish = function() {
+  if (this._state == RESOLVED && this._onResolve)
+    this._onResolve(this._value);
+  else if (this._state == REJECTED && this._onReject)
+    this._onReject(this._value);
+};
+
+
 Deferred.prototype.then = function(onResolve, onReject) {
-  if (this.onResolve != null || this.onReject != null)
+  if (this._onResolve != null || this._onReject != null)
     onReject(new Error('deferred has already been subscribed to'));
   else {
-    this.onResolve = onResolve;
-    this.onReject  = onReject;
-    this.publish();
+    this._onResolve = onResolve;
+    this._onReject  = onReject;
+    this._publish();
   }
 }
 
 Deferred.prototype.resolve = function(val) {
-  this.update(RESOLVED, val);
+  this._update(RESOLVED, val);
 };
 
 Deferred.prototype.reject = function(cause) {
-  this.update(REJECTED, cause);
+  this._update(REJECTED, cause);
 };
 
 Deferred.prototype.isResolved = function() {
-  return this.state != PENDING;
-};
-
-Deferred.prototype.update = function(state, val) {
-  if (this.isResolved())
-    throw new Error('deferred is already resolved');
-
-  this.state = state;
-  this.value = val;
-  this.publish();
-};
-
-Deferred.prototype.publish = function() {
-  if (this.state == RESOLVED && this.onResolve)
-    this.onResolve(this.value);
-  else if (this.state == REJECTED && this.onReject)
-    this.onReject(this.value);
+  return this._state != PENDING;
 };
 
 
