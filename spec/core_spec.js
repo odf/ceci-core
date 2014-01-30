d530cca140e2e6d8381474d4631a5ace50758264
odf <olaf.delgado@gmail.com>
2014-01-30 13:46:03 +1100

    add more tests for go blocks

--- spec/core_spec.js
+++ spec/core_spec.js
@@ -4,6 +4,24 @@ var cc = require('../index');
 
 
 describe('a go block', function() {
+  it('leaves plain values alone upon a yield', function(done) {
+    var val = { the: 'value' };
+
+    cc.go(function*() {
+      expect(yield val).toEqual(val);
+      done();
+    });
+  }),
+
+  it('passes extra arguments into its generator', function(done) {
+    cc.go(
+      function*(a, b) {
+        expect(a + b).toEqual(12);
+        done();
+      },
+      5, 7);
+  }),
+
   it('eventually returns the return value of its generator', function(done) {
     var val = { a: 'value' };
 
@@ -16,7 +34,7 @@ describe('a go block', function() {
     });
   });
 
-  it('passes out exceptions thrown in the generator', function(done) {
+  it('passes along uncaught exceptions from its generator', function(done) {
     var msg = 'Ouch!';
 
     cc.go(function*() {
@@ -31,6 +49,37 @@ describe('a go block', function() {
       }
 
       expect(thrown).toEqual(msg);
+      done();
+    });
+  });
+
+  it('supports arbitrary thenables in a yield', function(done) {
+    var val = { hello: 'val' };
+    var msg = 'Nah!';
+
+    var succeeding = {
+      then: function(onResolved, onReject) {
+        onResolved(val);
+      }
+    };
+
+    var failing = {
+      then: function(onResolved, onReject) {
+        onReject(msg);
+      }
+    };
+
+    cc.go(function*() {
+      var thrown = null;
+      try {
+        yield failing;
+      } catch(ex) {
+        thrown = ex;
+      }
+      expect(thrown).toEqual(msg);
+
+      expect(yield succeeding).toEqual(val);
+
       done();
     });
   });
