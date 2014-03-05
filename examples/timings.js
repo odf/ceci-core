02c03a307d42b6ca033a3aa6ff41a63f6bb138d6
odf <olaf.delgado@gmail.com>
2014-03-05 11:06:44 +1100

    add timings for deferreds and kew promises

--- examples/timings.js
+++ examples/timings.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var cc = require('../lib/index');
+var Q = require('kew');
+
 
 var runner = function(code) {
   return function() {
@@ -16,28 +18,61 @@ var runner = function(code) {
 };
 
 
+var check = function(n, r) {
+  var s = 0;
+  for (var i = 1; i < n; ++i)
+    s += i;
+  if (s != r)
+    console.log('Oops: expected ' + s + ', got ' + r);
+};
+
+
 cc.chain(
   null,
 
   runner(function*(done) {
-    var i, j;
+    var i, j = 0;
     for (i = 0; !done(); ++i)
-      j = (i % 50000 == 0) ? yield i : i;
-    console.log('loop executions per second: ' + i);
+      j += (i % 50000 == 0) ? yield i : i;
+    console.log('  loop executions per second: ' + i);
+    check(i, j);
   }),
 
   runner(function*(done) {
-    var i, j;
+    var i, j = 0;
     for (i = 0; !done(); ++i)
-      j = yield i;
-    console.log('         yields per second: ' + i);
+      j += yield i;
+    console.log('           yields per second: ' + i);
+    check(i, j);
   }),
 
   runner(function*(done) {
-    var i, j;
+    var i, j = 0, d;
+    for (i = 0; !done(); ++i) {
+      d = cc.defer();
+      d.resolve(i);
+      j += yield d;
+    }
+    console.log('deferreds yielded per second: ' + i);
+    check(i, j);
+  }),
+
+  runner(function*(done) {
+    var i, j = 0, p;
+    for (i = 0; !done(); ++i) {
+      p = Q.resolve(i);
+      j += yield p;
+    }
+    console.log(' promises yielded per second: ' + i);
+    check(i, j);
+  }),
+
+  runner(function*(done) {
+    var i, j = 0;
     var block = function*() { return i; };
     for (i = 0; !done(); ++i)
-      j = yield cc.go(block);
-    console.log('    go routines per second: ' + i);
+      j += yield cc.go(block);
+    console.log('      go routines per second: ' + i);
+    check(i, j);
   })
-);
+).then(null, function(ex) { throw ex; });
